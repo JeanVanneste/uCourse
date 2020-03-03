71ca022b1b636ca44fa36d64899d2e9bfeac0b16
Sébastien Combéfis <seb478@gmail.com>
2020-03-03 12:12:42 +0100

    Add code for AI611µ micro-course

---
+++ AI611µ/code/corpustats.py
@@ -0,0 +1,17 @@
+# corpustats.py
+# Author: Sébastien Combéfis
+# Version: March 2, 2020
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='corpus text file to analyse')
+    parser.add_argument('-o', '--out', metavar='PATH', help='path to the JSON file where to output the results')
+    args = parser.parse_args()
+
+    print('Analysing', args.file)
+    if args.out:
+        print('Writing output to', args.out)
+    else:
+        print('Writing output to stdout')
