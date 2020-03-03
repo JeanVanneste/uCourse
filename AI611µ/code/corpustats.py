201a0e015731d59053f4362a5d58424c776c1e63
Sébastien Combéfis <seb478@gmail.com>
2020-03-03 12:18:39 +0100

    Update code for the AI611µ micro-course

--- AI611µ/code/corpustats.py
+++ AI611µ/code/corpustats.py
@@ -7,7 +7,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='corpus text file to analyse')
-    parser.add_argument('-o', '--out', metavar='PATH', help='path to the JSON file where to output the results')
+    parser.add_argument('-o', '--out', metavar='PATH', help='path to the file where to output the result of the analysis')
     args = parser.parse_args()
 
     print('Analysing', args.file)
