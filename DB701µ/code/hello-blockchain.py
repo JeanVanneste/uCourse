b9ba21450d77682db73a18d67630d51647b55866
Sébastien Combéfis <seb478@gmail.com>
2020-04-23 13:16:40 +0200

    Add code for DB701µ micro-course

---
+++ DB701µ/code/hello-blockchain.py
@@ -0,0 +1,22 @@
+# hello-blockchain.py
+# Author: Sébastien Combéfis
+# Version: April 23, 2020
+
+class Block:
+    def __init__(self, index, timestamp, prevHash, data):
+        pass
+
+class BlockChain:
+    def __init__(self):
+        pass
+
+    def addData(self, data):
+        '''Add a new piece of data in this blockchain.'''
+        pass
+
+    def isValid(self):
+        '''Checks whether this blockchain is valid.'''
+        pass
+
+    def __str__(self):
+        pass
